1c2339d99a1e10f52d04cc680193d58b5fcb3313
Markus <muletto42@web.de>
2025-04-25 08:38:41 +0200

    commit

--- include/versions.h
+++ include/versions.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define MAIN_Version "66a7f6a"
+#define MAIN_Version "7eeaf17"
 #define KNX_Version "2.1.0+e69f636"
 #define MODULE_Common_Version "1.2.0+9f24e6e"
 #define MODULE_Common_Version_Major 1
