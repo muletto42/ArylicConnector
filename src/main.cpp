12be017a7cd116f9e0a8f4d8c85391f7093e2da2
Markus <mulett4@web.de>
2025-04-29 13:04:20 +0200

    Pin 11 und 12

--- src/main.cpp
+++ src/main.cpp
@@ -18,10 +18,10 @@
  * Serial2 is UART1 --> frei
  */
  #define ArylicUARTPORT Serial2 // Serial2 = UART1 //auto& ArylicUART = Serial1; //das geht auch als Namensgeber
- #define ARYLIC_TX_PIN 6 // UART1 TX mit Amp RX verbinden //GP4 
- #define ARYLIC_RX_PIN 7 // UART1 RX mit Amp TX verbinden //GP5 
-//#define ARYLIC_TX			11 // UART1 TX mit Amp RX verbinden //GP8 
-//#define ARYLIC_RX	    12 // UART1 RX mit Amp TX verbinden //GP9 
+ //#define ARYLIC_TX_PIN 6 // UART1 TX mit Amp RX verbinden //GP4 
+ //#define ARYLIC_RX_PIN 7 // UART1 RX mit Amp TX verbinden //GP5 
+#define ARYLIC_TX_PIN		11 // UART1 TX mit Amp RX verbinden //GP8 
+#define ARYLIC_RX_PIN	    12 // UART1 RX mit Amp TX verbinden //GP9 
 
 // ArylicUART arylic(ArylicUARTPORT, ARYLIC_TX_PIN, ARYLIC_RX_PIN);
 ArylicUART arylic(ArylicUARTPORT);
@@ -69,7 +69,7 @@ void setup()
 }
   
 unsigned long lastHeartbeat = 0;
-const unsigned long heartbeatInterval = 5000;
+const unsigned long heartbeatInterval = 10000;
 
 void loop()
 {
@@ -78,7 +78,7 @@ void loop()
 
     if (millis() - lastHeartbeat >= heartbeatInterval) 
     {
-      Serial.println("lebt");
+      Serial.println("lebt: gebe ein: 'vol <0-100>' oder 'src <id>'");
       lastHeartbeat = millis();
     }
 #endif
